5da209d945e398eddec8d43d816b469616322c5a
mcflav <52865683+mcflav@users.noreply.github.com>
2020-04-19 17:19:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function DownloadResume(){
       var resume = getElementById("position");
       var link = getElementById("download");
       if(resume == "sftWareDev"){
-        link.setAttribute("href", "resumes\softwareDeveloper.docx");
+        link.setAttribute("href", "softwareDeveloper.docx");
       }else if(resume == "frontendDev"){
-        link.setAttribute("href", "resumes\frontEndDeveloper.docx");    
+        link.setAttribute("href", "frontEndDeveloper.docx");    
       }else if(resume == "backendDev"){
-         link.setAttribute("href", "resumes\backEndDevloper.docx");   
+         link.setAttribute("href", "backEndDevloper.docx");   
       }
 }
