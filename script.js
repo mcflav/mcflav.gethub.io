25af096dc377977cb02f2498fc5ab51f151bd216
mcflav <52865683+mcflav@users.noreply.github.com>
2020-04-22 00:14:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ function DownloadResume(){
         link.setAttribute("href", "Jr Associate Level Developer.docx");
       }else if(resume.value == "dotNetDev"){
         link.setAttribute("href", "Jr .Net Software Developer.docx");    
-      }else if(resume.value == "backendDev"){
-         link.setAttribute("href", "backEndDeveloper.docx");   
+      }else if(resume.value == "frontendDev"){
+         link.setAttribute("href", "Front End Developer.docx");   
       }
 }
 
